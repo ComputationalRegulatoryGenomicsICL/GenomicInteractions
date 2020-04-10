a7fafe8edff32e8cce92211b9d4613f6caba0bf5
Liz Ing-Simmons <liz.ingsimmons@gmail.com>
2020-04-10 12:15:05 +0200

    Added description to package documentation; version bump.

--- R/GenomicInteractions-package.R
+++ R/GenomicInteractions-package.R
@@ -1,4 +1,8 @@
-#' GenomicInteractions: A package for analysing chromosome conformation
+#' GenomicInteractions
+#'
+#' Utilities for handling genomic interaction data such as
+#' ChIA-PET or Hi-C, annotating genomic features with interaction
+#' information, and producing plots and summary statistics.
 #'
 #' @name GenomicInteractions-package
 #' @rdname GenomicInteractions-package
